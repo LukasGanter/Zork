01caaa8f0370181334009cfdd7bca75a876d3965
lukas <lukasganter@gmail.com>
2024-05-20 22:46:04 +0200

    WIP: player implementation

--- Zork/player.cpp
+++ Zork/player.cpp
@@ -3,12 +3,44 @@
 Player::Player(std::string title, std::string description) :
 	Character(title, description)
 {
+	equiped_weapon = nullptr;
+	has_backpack = false;
+
 }
 
 Player::~Player()
 {
+	equiped_weapon = nullptr;
 }
 
 void Player::tick()
 {
 }
+
+bool Player::equip_weapon(Weapon* weapon)
+{
+	return false;
+}
+
+bool Player::unequip_weapon()
+{
+	return false;
+}
+
+bool Player::take_item(Item* item)
+{
+	return false;
+}
+
+bool Player::drop_inventory()
+{
+	return false;
+}
+
+void Player::search_inventory()
+{
+	for (Item* item : inventory) {
+		item->printInformation();
+	}
+}
+
--- Zork/player.h
+++ Zork/player.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "character.h"
+#include "weapon.h"
+#include <vector>
 
 class Player : public Character
 {
@@ -10,5 +12,18 @@ public:
 
 public:
 	void tick() override;
+
+	bool equip_weapon(Weapon* weapon);
+	bool unequip_weapon();
+	bool take_item(Item* item);
+	bool drop_inventory();
+
+	void search_inventory();
+
+private:
+
+	Weapon* equiped_weapon;
+	bool has_backpack;
+	std::vector<Item*> inventory;
 
 };
